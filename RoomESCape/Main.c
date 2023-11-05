c538545bd980ce6b622c1c4bdee4ab87a729a3fa
이윤석 <59646592+simplex3510@users.noreply.github.com>
2023-11-05 16:28:11 +0900

    Add cJson file

--- RoomESCape/Main.c
+++ RoomESCape/Main.c
@@ -1,6 +1,8 @@
-
+#include "Main.h"
+#include "cJson/cJSON.h"
+#include "cJson/cJSON_Utils.h"
 
 int main()
 {
-
+	cJSON* cJson = cJSON_Parse("asd");
 }
---
+++ RoomESCape/Main.h
@@ -0,0 +1,7 @@
+#pragma once
+#include<stdio.h>       //기본 입출력을 위한 라이브러리
+#include<conio.h>		//getch()함수를 쓰기위해 사용되는 라이브러리(키보드 입력값을 받을 때 사용)
+#include<Windows.h>		//글자색, 배경색을 바꾸기 위한 라이브러리
+#include<time.h>        //게임에 시간흐름을 나타내기 위한 라이브러리
+#include<stdlib.h>      // 난수를 발생시키기 위한 라이브러리
+#include<stdbool.h>     // 논리형 자료를 쓰기 위한 라이브러리
